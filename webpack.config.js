74329a322d4e95796f8bb79041fffb9dd4d1383c
marcinmdev <marcin@insanet.pl>
2022-07-06 14:51:45 +0200

    nginx docker compose fix n webpack extract sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './assets/app.js',
+  entry:  "./assets/app.js",
+  mode: 'production',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'public/build'),
+    filename: "main.js",
+    path:     path.resolve(__dirname, "public/build")
   },
+  plugins: [new MiniCssExtractPlugin({filename: "style.css"})],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use:  [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
 };
